20ef7a409196437092c06dbc34acf3630171fb57
Camillo Camarda <camillo.camarda@ITA00525.fritz.box>
2022-09-11 17:07:25 +0200

    debug

--- data/postprocess.js
+++ data/postprocess.js
@@ -26,6 +26,7 @@ for (let i = 0; i < tokenIdsToRefetch.length; i++) {
             +"&tokenId="+tokenId
             +"&tokenUriTimeoutInMs=0";
             
+    console.log("fetching ",fetchUrl)
     const response = await fetch(fetchUrl);
     const element = await response.json();
     savedMetadata.nft_metadata[tokenId] =  element.metadata;
